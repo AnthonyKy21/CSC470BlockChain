b7e24d616d3f9934bd14a1546c3efc2ec27cc683
Ky <Ky@10.0.0.160>
2018-05-01 01:28:59 -0400

    1. Added a controller class 2. Adding a JFrame because last one got ruined?

---
+++ controller/Controller.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import views.Frame;
+
+/**
+ *
+ * @author Ky
+ */
+public class Controller {
+    
+    private final Frame frame = new Frame();
+    
+    public Controller() {
+    frame.setTitle(getClass().getSimpleName());
+    frame.setLocationRelativeTo(null);
+}
+    
+    public static void main(String[] args) {
+        Controller app = new Controller();
+        app.frame.setVisible(true);
+    }
+    
+}
