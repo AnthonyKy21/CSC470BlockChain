7d373bbbb0af7d188c38d2a9a555d64f5c4cf9f4
Joseph Jaspers <jj804785@wcupa.edu>
2018-05-11 13:24:52 -0400

    doc

--- Forwarder.java
+++ Forwarder.java
@@ -11,6 +11,12 @@ import java.io.Serializable;
  *
  * @author joseph
  * @param <T>
+ * 
+ * -- A lamda class
+ * 
+ * Trivial interface for Nodes. The forward method effecively works on controlling what a Node does 
+ * after it successfully listens and pickups any data sent to it. Various implementations of this 
+ * work to send the data to the apropriate structures/methods of the block chain.
  */
 public interface Forwarder<T extends Serializable> {    
     void forward(T data)  throws InterruptedException;
