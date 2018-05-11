76348b3c64ce4a6722fff52305e2214e7449622c
Joseph Jaspers <jj804785@wcupa.edu>
2018-05-11 13:17:57 -0400

    Improve documentation

--- BlockChain.java
+++ BlockChain.java
@@ -15,8 +15,11 @@ import java.util.Date;
 import java.util.LinkedList;
 /**
  *
- * @author joseph
- * @param <T>
+ * @author joseph jaspers, won murdoc, anthony ky
+ * documentation: Joseph Jaspers       
+ * 
+ *  Block chain implementation P2P Network
+ * 
  */
 public class BlockChain<T extends Serializable> implements Serializable {
     
@@ -26,6 +29,7 @@ public class BlockChain<T extends Serializable> implements Serializable {
     final int HOST_IP_SNDR  = 9089;
     final boolean HOST;
 
+    //A data type containing individual elements of type T and relevant information
     protected final class data implements Serializable {
         
         int hash = 0;
@@ -55,16 +59,16 @@ public class BlockChain<T extends Serializable> implements Serializable {
         }
     }
     
-    LinkedList<data> list = new LinkedList<>();
+    LinkedList<data> list = new LinkedList<>();//The actual blockchain
     
-    Node<T> node;
-    Node<LinkedList<data>> verifier;
-    Node<ArrayList<String>> ip_receiver;
-    Node<String> host_linker; 
-    ArrayList<String> addresslist;
+    Node<T> node;                              //Listener/Sender for the Generic T type
+    Node<LinkedList<data>> verifier;           //Listever/Sender for sending/receiving the actual chain, handles discrepencies between 2 or more blockchains.
+    Node<ArrayList<String>> ip_receiver;       //IP address listener used for listening for when a pseudo-host adds a new connection to the network
+    Node<String> host_linker;                  //Initialzied by Pseudo-Host-Nodes only,listens for an IP-address string, and pings all other nodes, informing them to add the IP to list.
+    ArrayList<String> addresslist;             //List of known IP-addresses on the network 
     
     BlockChain() {
-        HOST = false;
+        HOST = false;      
         node = new Node<>(SENDER_PORT, new Forward_Adder<>(this));
         verifier = new Node<>(VERIFIER_PORT, new Forward_Setter<>(this));
         ip_receiver = new Node<>(IP_PORT, new Forward_IP(this));
@@ -78,7 +82,8 @@ public class BlockChain<T extends Serializable> implements Serializable {
        if (isHost)
            host_linker = new Node<>(HOST_IP_SNDR, new Forward_InitialConnect(this));
     }
-    //initializes the nodes 
+    
+    //initializes the nodes to start listening to the network
     public void init() {
         node.start();
         verifier.start();
@@ -87,7 +92,7 @@ public class BlockChain<T extends Serializable> implements Serializable {
         if (HOST)
             host_linker.start();
     }   
-    //Add an ip address to the nodes
+    //Add an ip address to the nodes, pings any added connection to the rest of the network
     public void addConnection(String ip) {
         node.addConnection(ip);
         verifier.addConnection(ip);
@@ -101,6 +106,7 @@ public class BlockChain<T extends Serializable> implements Serializable {
     }
     
     //adds a connection without pinging the connection to everyone
+    //this is utilized for when another node pings an IP to another node (as to avoid pinging the same IP indefinately)
     public void safe_addConnection(String ip) {
         node.addConnection(ip);
         verifier.addConnection(ip);
@@ -113,6 +119,7 @@ public class BlockChain<T extends Serializable> implements Serializable {
     
     
     //Attempts to connect to a pseudo-host -- the host will add the ip to its config and ping back the iplist to the client 
+    //Connect to a pseudo-host to have the host ping your IP to the rest of the network and add yourself to the network
     void initialConnect(String serverAddress, String ip_self) {
         try(Socket socket = new Socket(serverAddress, HOST_IP_SNDR))
         {
@@ -134,13 +141,14 @@ public class BlockChain<T extends Serializable> implements Serializable {
         return list.size();
     }
     
-    //add data to the blockchain and to everyone on the net 
+    //add data to the blockchain and to everyone on the net (automatically sends to the network)
     void add(T info) throws InterruptedException {        
         list.add(new data(info));        
         node.send(info);
         verifier.send(list);
     }
     //add data to the blockchain without pinging (used for updating during collisions via Forward_Setter)
+    //Similair to safe_addConnection()
     void safe_add(T info) {
         list.add(new data(info)); 
     }
@@ -161,7 +169,7 @@ public class BlockChain<T extends Serializable> implements Serializable {
         return str;
     }
     
-    //checks if a block is correct
+    //checks if a blockchain has correct hashes / is a valid blockchain
     public boolean validate(LinkedList<data> list) {
         
         int hash = 0;
